3e85109bd12eaf8ab5c4e639b3a99f9b92e3af0b
Baris Macaroglu <barismacaroglu@hotmail.com>
2021-09-09 11:04:44 +0200

    function to get all of the articles from API via xhr

--- front-end/scripts/index.js
+++ front-end/scripts/index.js
@@ -1,1 +1,29 @@
 console.log("Script chargé !");
+
+bringAllTeddies();
+function bringAllTeddies() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET","http://localhost:3000/api/teddies");
+  xhr.onload = function() {
+    document.getElementById("teddies").innerHTML = "";
+    let teddiesList = document.getElementById("teddies");
+    if(this.status == 200) {
+      console.log(this.responseText); // Affiche tous les teddies en forme de texte / string.
+      const teddies = JSON.parse(this.responseText); // Converts string to JSON
+      console.log(teddies); // Affiche tous les teddies sous forme d'Array sur la console.
+      teddies.forEach(function(teddy) {
+        teddiesList.innerHTML += `
+        <tr>
+            <td id = "${teddy._id}" class="teddyIdClass">${teddy._id}</td>
+            <td>${teddy.name}</td>
+            <td>${teddy.price}</td>
+            <td> <img src="${teddy.imageUrl}" class = "img-teddy"> </td>
+            <td><button class="selectThisTeddy">Sélectionner</button></td>
+        </tr> `;
+        const teddyId = document.getElementById(`${teddy._id}`);
+        console.log(teddyId.id);
+      });
+    }
+  }
+  xhr.send();
+}
