977574c2697732aa59d16e528ac53528586b8911
Baris Macaroglu <barismacaroglu@hotmail.com>
2021-09-14 15:55:45 +0200

    Getting the articles from API via Fetch

--- front-end/scripts/index.js
+++ front-end/scripts/index.js
@@ -29,10 +29,11 @@ function bringAllTeddies() {
 }
 
 
-getArticles();
+// getArticlesXHR();
+getArticlesFetch();
 
 //Récupérer les articles depuis l'API via xhr
-function getArticles() {
+function getArticlesXHR() {
     const xhr = new XMLHttpRequest();
     xhr.open("GET","http://localhost:3000/api/teddies");
     xhr.onload = function() {
@@ -77,4 +78,64 @@ function getArticles() {
       }
     }
     xhr.send();
-  }
+}
+
+//Récupérer les articles depuis l'API via fetch
+function getArticlesFetch() {
+  fetch("http://localhost:3000/api/teddies")
+  .then(function(res) {
+    return res.json();
+  })
+  .catch((error) => {
+    let productsContainer = document.querySelector("#products__container");
+    productsContainer.innerHTML = "Problème de serveur";
+    productsContainer.style.padding = "40px";
+  })
+
+  // Dispatcher les données de chaque produit (nom, image, prix etc.)
+  .then(function(resultsAPI) {
+    const articles = resultsAPI;
+    console.log(articles);
+
+    for(let article in articles) {
+
+      let productCard = document.createElement("div");
+      document.querySelector(".products").appendChild(productCard);
+      productCard.classList.add("product");
+
+      let productLink = document.createElement("a");
+      productCard.appendChild(productLink);
+      //Question sur l'attribue des href !!!
+      productLink.href = `product.html?id=${resultsAPI[article]._id}`;
+
+      let productImgDiv = document.createElement("div");
+      productLink.appendChild(productImgDiv);
+      productImgDiv.classList.add("product__image");
+
+      let productImg = document.createElement("img");
+      productImgDiv.appendChild(productImg);
+      productImg.src = resultsAPI[article].imageUrl;
+
+      let productInfo = document.createElement("div");
+      productLink.appendChild(productInfo);
+      productInfo.classList.add("product__info");
+
+      let productName = document.createElement("p");
+      productInfo.appendChild(productName);
+      productName.classList.add("product__info--name");
+      productName.innerHTML = resultsAPI[article].name;
+
+      let productPrice = document.createElement("p");
+      productInfo.appendChild(productPrice);
+      productPrice.classList.add("product__info--price");
+      // productPrice.innerHTML = (resultsAPI[article].price)/100 + " €";
+
+      resultsAPI[article].price = (resultsAPI[article].price) / 100;
+      productPrice.innerHTML = new Intl.NumberFormat("fr-FR", {
+        style: "currency",
+        currency: "EUR",
+      }).format(resultsAPI[article].price);
+      
+    }
+  })
+}
