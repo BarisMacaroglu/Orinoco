dfd3e124f18d5771580336b83c71d23b643575ce
Baris Macaroglu <barismacaroglu@hotmail.com>
2021-09-14 22:27:25 +0200

    Local Storage init : function getItemsFromLS() created

--- front-end/scripts/product.js
+++ front-end/scripts/product.js
@@ -61,3 +61,20 @@ function getArticle(id) {
       }
     });
   }
+
+  // -------- Local Storage ----------
+
+  function getItemsFromLS() {
+    let itemsArray;
+
+    //Si un Array de 'itemsArray' n'existe pas sur Local Storage, on l'initialise :
+    if(localStorage.getItem("itemsArray") === null) {
+      itemsArray = [];
+    }
+    //Sinon, on récupère l'Array 'itemsArray' qui existe déjà sur Local Storage
+    else {
+      // localStorage.getItem("itemsArray") -> est un String, il faut que l'on le transforme à l'Array :
+      itemsArray = JSON.parse(localStorage.getItem("itemsArray"));
+    }
+    return itemsArray;
+  }
