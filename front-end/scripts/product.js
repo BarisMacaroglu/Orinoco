1d15236e725bc26411e94f94f886e7f2f638cfb6
Baris Macaroglu <barismacaroglu@hotmail.com>
2021-09-14 19:12:53 +0200

    Selecting HTML elements and getting the infos of the selected product from API via fetch

--- front-end/scripts/product.js
+++ front-end/scripts/product.js
@@ -1,1 +1,63 @@
 console.log("Script chargé !");
+
+let params = new URL(document.location).searchParams;
+let id = params.get("id");
+
+// Pour donner le nom du produit à la page :
+const pageTitle = document.querySelector("title");
+
+const container = document.querySelector(".container");
+const productDiv = document.querySelector(".product");
+const productImg = document.querySelector(".img");
+const productName = document.querySelector(".product__name");
+const productPrice = document.querySelector(".product__price");
+const productDescription = document.querySelector(".product__description");
+const productColor = document.querySelector("#color");
+const productQuantity = document.querySelector("#quantity");
+
+const addToBasketBtn = document.querySelector(".add-to-basket");
+const successMsg = document.querySelector(".added__msg");
+const successMsgText = document.querySelector(".added__msg--text");
+
+getArticle(id);
+
+// La fonction pour voir les détailles du produit choisi sur sa propre page :
+function getArticle(id) {
+  fetch(`http://localhost:3000/api/teddies/${id}`)
+  
+    //   .then(function (response) {
+    //     console.log(response); //Logs a response, type 'cors'
+    //     return response.json();
+    //   })
+    
+    .then(response => response.json())
+    
+    .catch((error) => {
+      console.log(error);
+      container.innerHTML = "Problème de serveur";
+      container.style.textAlign = "center";
+      container.style.padding = "40px";
+    })
+    
+    // Récuperer et afficher les infos du produit choisi
+    .then(article => {
+      // console.log(article);
+      pageTitle.innerHTML = article.name + " | Orinoco";
+      productDiv.id = article._id;
+      productName.innerHTML = article.name;
+      productDescription.innerHTML = article.description;
+      productImg.src = article.imageUrl;
+      // productPrice.innerHTML = article.price/100 + " €"
+      article.price = article.price / 100;
+      productPrice.innerHTML = new Intl.NumberFormat("fr-FR", {
+        style: "currency",
+        currency: "EUR",
+      }).format(article.price);
+      
+      for(let i = 0; i < article.colors.length; i++) {
+        let colorOption = document.createElement("option");
+        colorOption.innerHTML = article.colors[i];
+        productColor.appendChild(colorOption);
+      }
+    });
+  }
