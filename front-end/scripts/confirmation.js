af42fbd5f297d989f2dce63cacaadf7840747dc3
Baris Macaroglu <barismacaroglu@hotmail.com>
2021-09-27 09:54:08 +0200

    Confirmation page finished

--- front-end/scripts/confirmation.js
+++ front-end/scripts/confirmation.js
@@ -1,1 +1,9 @@
 console.log("Script chargé !");
+
+const orderId = document.querySelector(".order-id");
+const totalPrice = document.querySelector(".total-price");
+
+orderId.innerHTML = localStorage.getItem("orderId");
+totalPrice.innerHTML = localStorage.getItem("total");
+
+// localStorage.clear();
